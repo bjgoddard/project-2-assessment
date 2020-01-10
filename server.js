929b0f1e51dfaeaa602087b4f49e494983a2d7a4
bjgoddard <bjgoddard94@gmail.com>
2020-01-10 14:45:35 -0800

    css is dum

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override');
-
 const app = express();
+let db = require('./models')
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
@@ -9,6 +9,49 @@ app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
+
+//GET
+app.get('/', (req, res) => {
+    db.widget.findAll()
+    .then(widgets => {
+    res.render('index', { widgets })
+    }) 
+})
+
+app.post('/', (req, res) => {
+    db.widget.create({
+        description: req.body.description,
+        quantity: req.body.quantity
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+})
+
+app.delete('/', (req, res) => {
+    db.widget.destroy({
+        where: {id: req.body.widgetdelete}
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+})
+//DELETE
+// router.delete('/:id', isLoggedIn, (req, res) => {
+//     db.teams.findOne({
+//         where: { id : req.params.id },
+//         include: [db.members]
+//     })
+//     .then(() => {
+//         db.members.destroy({ 
+//             where: { id: req.body.memberName }  
+//         })
+//     })
+//         .then(() => {
+//             res.redirect(`/profile/${req.params.id}`)
+//         })
+//     })
+
 
 
 
